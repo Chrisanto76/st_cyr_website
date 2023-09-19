0733a03ceba5e856628ca109bd5a02a74d8453cd
Chrisanto76 <antonypoure2@gmail.com>
2023-09-19 16:35:25 +0200

    add Hero section

---
+++ src/pages/Home/Hero/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => {
+	return (
+		<div className="hero--img container">
+			<div className="hero--content">
+				<h1 className="heading--primary">
+					<span>Bienvenue</span> au Saint Cyr
+				</h1>
+
+				<p className="text--white">
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+				</p>
+
+				<p className="text--white">
+					Reservez en ligne, ou appelez{' '}
+					<span className="special--word"> 01 45 79 80 42</span>
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default Hero;
