4fe9402471311ee937441151d5e2b35f044bbce0
Chrisanto76 <antonypoure2@gmail.com>
2023-09-20 12:15:54 +0200

    add MenuOfTheDay folder

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import HeroImage from '../../components/HeroImage/HeroImage';
+import bgimage from '../../assets/coffee.jpg';
+import MenuOfTheDay from './MenuOfTheDay/MenuOfTheDay';
 
 const Menu = () => {
-	return <div>Menu</div>;
+	return (
+		<div>
+			<HeroImage
+				bgimage={bgimage}
+				heading={['Notre ', <span>Menu</span>]}
+				text="Plats faits maison, cuisine de saison."
+			/>
+			<MenuOfTheDay />
+		</div>
+	);
 };
 
 export default Menu;
---
+++ src/pages/Menu/MenuOfTheDay/MenuOfTheDay.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import ListItem from '../../../components/ListItem/ListItem';
+import './menuOfTheDay.css';
+
+import image1 from '../../../assets/burger.jpeg';
+import image2 from '../../../assets/pizza.jpeg';
+
+const menuData = [
+	{
+		title: 'Burger',
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+		price: '15€',
+		itemImage: image1,
+	},
+	{
+		title: 'Pizza',
+		description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+		price: '15€',
+		itemImage: image2,
+	},
+];
+
+const MenuOfTheDay = () => {
+	return (
+		<div className="section">
+			<div className="container menu-of-the-day">
+				<h1 className="heading--secondary">
+					<span>Menu </span>du jour
+				</h1>
+				{menuData.map((data, i) => (
+					<ListItem
+						title={data.title}
+						description={data.description}
+						price={data.price}
+						itemImage={data.itemImage}
+					/>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default MenuOfTheDay;
